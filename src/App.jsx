4e3055c944955fc09e8c5337c4a4f1097f5cf36d
giva16 <ltpie1@hotmail.com>
2024-03-29 18:41:59 +0100

    Cleaned up App.jsx, moved data to data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,34 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
-import koreanFood from './assets/korean-food.webp';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import data from './data';
 import 'swiper/css';
 
 function App() {
+  const cards = data.map((exp) => {
+    return (
+      <SwiperSlide key={1}>
+        <Card
+          status={exp.status}
+          image={{ img: exp.image.img, alt: exp.image.alt }}
+          rating={exp.rating}
+          numReviews={exp.numReviews}
+          location={exp.location}
+          title={exp.title}
+          price={exp.price}
+        />
+      </SwiperSlide>
+    );
+  });
   return (
     <div className='container'>
       <Navbar />
       <main>
         <Hero />
         <div className='experience-cards'>
-          <Swiper spaceBetween={0} slidesPerView={3} onSlideChange={() => console.log('slide change')} onSwiper={(swiper) => console.log(swiper)}>
-            <SwiperSlide>
-              <Card
-                status='Online'
-                image={{ img: koreanFood, alt: 'Korean Bibimbap dish with eggs, beansprouts and various pickled vegetables' }}
-                rating={4.98}
-                numReviews={1024}
-                location='United Kingdom'
-                title='Korean Food Revolution'
-                price={22}
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Card
-                status='Sold Out'
-                image={{
-                  img: 'https://a0.muscache.com/im/pictures/lombard/MtTemplate-1762373-media_library/original/cee26245-7b4a-41f6-9615-57558f1887cd.jpeg?im_w=320',
-                  alt: "Blue Crane, South Africa's national bird",
-                }}
-                rating={4.93}
-                numReviews={786}
-                location='South Africa'
-                title='Live Virtual Wildlife Safari in South Africa'
-                price={208}
-                per='group'
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Card
-                status='Online'
-                image={{
-                  img: 'https://a0.muscache.com/im/pictures/lombard/MtTemplate-1716598-media_library/original/86d22579-14c7-42b9-ac0b-f39b4d785b8d.jpeg?im_w=320',
-                  alt: 'Lebanese Humus Dish',
-                }}
-                rating={5.0}
-                numReviews={275}
-                location='Canada'
-                title='Making Hummus with a Lebanese Chef'
-                price={127}
-                per='group'
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Card
-                status='Online'
-                image={{
-                  img: 'https://a0.muscache.com/im/pictures/lombard/MtTemplate-5122832-media_library/original/c389daab-d4d7-43ef-bad3-9cdd3a6ce4ac.jpg?im_w=1440',
-                  alt: 'Japanese Geisha',
-                }}
-                rating={5.0}
-                numReviews={3}
-                location='Japan'
-                title='Make original itinerary in Kyoto with Akari'
-                price={42}
-                per='group'
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <Card
-                status='Online'
-                image={{
-                  img: 'https://a0.muscache.com/im/pictures/lombard/MtTemplate-5121505-media_library/original/62ad1850-896e-4f77-b276-0c19293c55bb.jpeg?im_w=320',
-                  alt: 'A street in Tokyo at night',
-                }}
-                rating={5.0}
-                numReviews={59}
-                location='Japan'
-                title='Plan the perfect Customised Japan Trip with Local Expert'
-                price={42}
-                per='group'
-              />
-            </SwiperSlide>
+          <Swiper spaceBetween={20} loop={true} slidesPerView={3} freeMode={true}>
+            {cards}
           </Swiper>
         </div>
       </main>
---
+++ src/data.js
@@ -0,0 +1,66 @@
+export default [
+  {
+    status: 'Sold Out',
+    image: {
+      img: 'https://a0.muscache.com/im/pictures/lombard/MtTemplate-1762373-media_library/original/cee26245-7b4a-41f6-9615-57558f1887cd.jpeg?im_w=320',
+      alt: "Blue Crane, South Africa's national bird",
+    },
+    rating: 4.9,
+    numReviews: 78,
+    location: 'South Africa',
+    title: 'Live Virtual Wildlife Safari in South Africa',
+    price: 20,
+    per: 'group',
+  },
+  {
+    status: 'Online',
+    image: {
+      img: 'https://recipetineats.com/wp-content/uploads/2019/05/Bibimbap_3.jpg',
+      alt: 'Korean Bibimbap dish with eggs, beansprouts and various pickled vegetables',
+    },
+    rating: 4.98,
+    numReviews: 1024,
+    location: 'United Kingdom',
+    title: 'Korean Food Revolution',
+    price: 22,
+  },
+  {
+    status: 'Online',
+    image: {
+      img: 'https://a0.muscache.com/im/pictures/lombard/MtTemplate-1716598-media_library/original/86d22579-14c7-42b9-ac0b-f39b4d785b8d.jpeg?im_w=320',
+      alt: 'Lebanese Humus Dish',
+    },
+    rating: 5.0,
+    numReviews: 275,
+    location: 'Canada',
+    title: 'Making Hummus with a Lebanese Chef',
+    price: 127,
+    per: 'group',
+  },
+  {
+    status: 'Online',
+    image: {
+      img: 'https://a0.muscache.com/im/pictures/lombard/MtTemplate-5122832-media_library/original/c389daab-d4d7-43ef-bad3-9cdd3a6ce4ac.jpg?im_w=1440',
+      alt: 'Japanese Geisha',
+    },
+    rating: 5.0,
+    numReviews: 3,
+    location: 'Japan',
+    title: 'Make original itinerary in Kyoto with Akari',
+    price: 42,
+    per: 'group',
+  },
+  {
+    status: 'Online',
+    image: {
+      img: 'https://a0.muscache.com/im/pictures/lombard/MtTemplate-5121505-media_library/original/62ad1850-896e-4f77-b276-0c19293c55bb.jpeg?im_w=320',
+      alt: 'A street in Tokyo at night',
+    },
+    rating: 5.0,
+    numReviews: 59,
+    location: 'Japan',
+    title: 'Plan the perfect Customised Japan Trip with Local Expert',
+    price: 42,
+    per: 'group',
+  },
+];
